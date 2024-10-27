393749ef7f280d3e9ebf3e8a93b25bced90c3178
Zirui <76266580+ziruiwang333@users.noreply.github.com>
2024-10-27 21:08:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogflX6dvoJOSlQslEHSUXJfLsAOQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	J1NkeQl1BtaZ7dCDLAK2xSm2VnNFLq3RQ3AvZeDzk-E',
+      useTemplateId: 'J1NkeQl1BtaZ7dCDLAK2xSm2VnNFLq3RQ3AvZeDzk-E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
