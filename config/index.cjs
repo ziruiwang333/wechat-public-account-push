9cfa8cc04f67e2b2ab44a047cba259aa8d118eed
Zirui <76266580+ziruiwang333@users.noreply.github.com>
2025-01-15 22:34:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogflX6dvoJOSlQslEHSUXJfLsAOQ',
+      id: 'ogflX6R6bGMQnaZ8_dICHHI-WTto',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'o8ZaVeCvsi9AgyM5Yh9_wTBCBT1gwuXENzFzqxH20N8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
